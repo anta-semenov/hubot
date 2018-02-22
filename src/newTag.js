132dd58b00fb189f8049ab34dc3a3619c6722b0d
Anton Semenov <anta.semenov@gmail.com>
2018-02-22 22:31:54 +0400

    add new tag script

--- src/newTag.js
+++ src/newTag.js
@@ -1,5 +1,30 @@
+import request from 'request'
+
 module.exports = (robot) => {
-  robot.respond(/create new.?tag/i, () => {
-    
+  robot.respond(/create new.?tag (patch|minor|major)/i, ({match}) => {
+    const data = {
+      hook_info: {
+        type: 'bitrise',
+        api_token: 'p5Zc51N880paoDRI9QKmPg'
+      },
+      build_params: {
+        branch: 'master',
+        workflow_id: 'newTag',
+        environments: [{
+          mapped_to: 'VERSION',
+          value: match[1],
+          is_expand: true
+        }]
+      },
+      triggered_by: 'curl'
+    }
+
+    const options = {
+      url: 'https://www.bitrise.io/app/145eef6440650dec/build/start.json',
+      method: 'POST',
+      body: JSON.stringify(data)
+    }
+
+    request(options)
   })
 }
