e217f14e08fc27d82c42a059717cab5c4a11972c
Anton Semenov <anta.semenov@gmail.com>
2018-02-24 15:49:21 +0400

    update new tag script

--- src/newTag.js
+++ src/newTag.js
@@ -1,7 +1,5 @@
-import request from 'request'
-
 module.exports = (robot) => {
-  robot.respond(/create new.?tag (patch|minor|major)/i, ({match}) => {
+  robot.respond(/create new.?tag (patch|minor|major)/i, ({match, send}) => {
     const data = {
       hook_info: {
         type: 'bitrise',
@@ -25,6 +23,17 @@ module.exports = (robot) => {
       body: JSON.stringify(data)
     }
 
-    request(options)
+    robot.http('https://www.bitrise.io/app/145eef6440650dec/build/start.json')
+    .header('Content-Type', 'application/json')
+    .post(JSON.stringify(data), () => {
+      const tagUrl = 'https://anta-semenov:43df8ab557838978806ff430105455582adbc6ba@api.github.com/repos/anta-semenov/hubot/tags'
+      robot.http(tagUrl).get((err, response, body) => {
+        if (err) return
+
+        const tagResponse = JSON.parse(body)
+
+        send(`new tag is ${tagResponse[0].name}`)
+      })
+    })
   })
 }
